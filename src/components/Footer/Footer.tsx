8097ff6a44f9e4389cc377a28c3ef197ca178941
shadow <lightshadow416@gmail.com>
2024-01-27 16:10:53 +0100

    [Footer] component : replacing social icons with new icons to solve a problem

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,14 @@
 import "./footer.css";
 // mui
 import { Stack } from "@mui/material";
+//
+import { ReactSVG } from "react-svg";
 // assets
-import facebook_logo from "/icon-facebook.svg";
-import youtube_logo from "/icon-youtube.svg";
-import twitter_logo from "/icon-twitter.svg";
-import pinterest_logo from "/icon-pinterest.svg";
-import instagram_logo from "/icon-instagram.svg";
+import facebook_logo from "/facebook.svg";
+import youtube_logo from "/square-youtube.svg";
+import twitter_logo from "/twitter.svg";
+import pinterest_logo from "/pinterest.svg";
+import instagram_logo from "/instagram.svg";
 
 export default function Footer() {
   interface socialLinks_type {
@@ -37,16 +39,15 @@ export default function Footer() {
               component="ul"
               direction="row"
               alignItems="center"
-              className="lg:space-x-[1rem] space-x-[2rem]"
+              className="lg:space-x-[1rem] space-x-[2rem] "
             >
               {socialLinks.map((e, i) => {
                 return (
                   <li key={i}>
                     <a href="#" aria-label={`follow me on ${e.label}`}>
-                      <img
+                      <ReactSVG
                         src={e.img}
-                        alt="img"
-                        className="max-lg:h-[2.7rem]"
+                        className=" max-lg:w-[2.7rem] h-full w-[2rem] fill-white hover:fill-BrightRed"
                       />
                     </a>
                   </li>
@@ -64,7 +65,11 @@ export default function Footer() {
                   spacing="1.2rem"
                 >
                   {e.map((element, index) => {
-                    return <li key={index}>{element}</li>;
+                    return (
+                      <li key={index} className="hover:text-BrightRed">
+                        {element}
+                      </li>
+                    );
                   })}
                 </Stack>
               );
@@ -137,6 +142,20 @@ let Logo = () => {
             />
           </g>
         </g>
+      </svg>
+    </>
+  );
+};
+
+let YoutubeLogo = () => {
+  return (
+    <>
+      <svg
+        className="h-[2rem] w-[2rem] fill-white hover:fill-BrightRed"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+      >
+        <path d="M282 256.2l-95.2-54.1V310.3L282 256.2zM384 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64zm14.4 136.1c7.6 28.6 7.6 88.2 7.6 88.2s0 59.6-7.6 88.1c-4.2 15.8-16.5 27.7-32.2 31.9C337.9 384 224 384 224 384s-113.9 0-142.2-7.6c-15.7-4.2-28-16.1-32.2-31.9C42 315.9 42 256.3 42 256.3s0-59.7 7.6-88.2c4.2-15.8 16.5-28.2 32.2-32.4C110.1 128 224 128 224 128s113.9 0 142.2 7.7c15.7 4.2 28 16.6 32.2 32.4z" />
       </svg>
     </>
   );
