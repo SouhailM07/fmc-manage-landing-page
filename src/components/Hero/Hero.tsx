1a7c93e06de6cc353d5325e517ad57b81419b171
shadow <lightshadow416@gmail.com>
2024-01-27 21:24:22 +0100

    adding closer solution for mobile bg

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -26,7 +26,7 @@ export default function Hero() {
           </button>
         </section>
         <section id="Hero__s2">
-          <img src={hero_lg} alt="image" fetchPriority="high" />
+          <img id="test" src={hero_lg} alt="image" fetchPriority="high" />
         </section>
       </article>
     </>
