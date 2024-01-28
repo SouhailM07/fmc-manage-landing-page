7d8eab9e3375098a620de7e55b4f503260c60810
shadow <lightshadow416@gmail.com>
2024-01-29 00:01:58 +0100

    adding [/Helmet/] component to the project

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import "./style/input.css";
 import "./App.css";
 // components
 import { MyContainer } from "./components";
-
+//
+import { Helmet } from "react-helmet";
 function App() {
   return (
     <>
+      <Helmet>
+        <meta name="description" content="this a front end mentor challenge" />
+        <meta name="keywords" content="front end challenges , landing page" />
+        <meta name="author" content="Shadow" />
+      </Helmet>
       <MyContainer />
     </>
   );
